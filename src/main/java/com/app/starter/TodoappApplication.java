168062bfc4d35295d99078e3dcd04494a08a35ee
sBugraTiryaki <sbugratiryaki@gmail.com>
2025-05-19 08:36:04 +0300

    add TaskRepository annotation, update TodoappApplication configuration, and create tasks.html template

--- src/main/java/com/app/starter/TodoappApplication.java
+++ src/main/java/com/app/starter/TodoappApplication.java
@@ -3,9 +3,13 @@ package com.app.starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.app.models")
+@ComponentScan(basePackages = {"com.app.controller", "com.app.service", "com.app.starter"})
+@EnableJpaRepositories(basePackages = "com.app.repository")
 public class TodoappApplication {
 
 	public static void main(String[] args) {
