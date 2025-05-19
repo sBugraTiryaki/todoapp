35be36dd76e2ee357eb956ba7269a391df7a2b22
sBugraTiryaki <sbugratiryaki@gmail.com>
2025-05-19 09:35:22 +0300

    add HTTP method override support and enhance task management UI with Bootstrap styling

--- src/main/java/com/app/starter/TodoappApplication.java
+++ src/main/java/com/app/starter/TodoappApplication.java
@@ -3,8 +3,10 @@ package com.app.starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.app.models")
@@ -15,5 +17,9 @@ public class TodoappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoappApplication.class, args);
 	}
-
+	
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }
